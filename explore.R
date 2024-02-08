3056f9fa3766a6b7a3fd1e6e810315a0898dad84
Corina Ramont <corinaramont@tamu.edu>
2024-02-08 10:37:58 -0600

    load function, add dataset path

--- explore.R
+++ explore.R
@@ -1,2 +1,6 @@
 library(dplyr)
 
+source("Functions/read_multiple_sheets.R") # load function
+
+path = "/Users/corinaramont/Library/CloudStorage/OneDrive-Personal/stat 692/FoodAccessResearchAtlasData2019.xlsx"
+data = ((multiplesheets(path))$`Food Access Research Atlas`) 
