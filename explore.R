087ad881c5b94eafd008cb8bad56c01d1e41585e
Corina Ramont <corinaramont@tamu.edu>
2024-02-08 10:39:44 -0600

    fix load function

--- explore.R
+++ explore.R
@@ -1,6 +1,7 @@
 library(dplyr)
 
-source("Functions/read_multiple_sheets.R") # load function
+source("read_multiple_sheets.R") # load function
 
+# load on macOS
 path = "/Users/corinaramont/Library/CloudStorage/OneDrive-Personal/stat 692/FoodAccessResearchAtlasData2019.xlsx"
 data = ((multiplesheets(path))$`Food Access Research Atlas`) 
