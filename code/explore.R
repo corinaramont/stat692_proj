77d8d55ad92cc48ea84b5f493bd25e9e04744409
Corina Ramont <corinaramont@tamu.edu>
2024-02-10 20:23:54 -0600

    change file path

--- code/explore.R
+++ code/explore.R
@@ -3,15 +3,17 @@ library(dplyr)
 source("functions/read_multiple_sheets.R") # load function
 
 # load on macOS
-path = "datasets/class_only_test.xlsx"
+path = "datasets/hai_only.xlsx"
 data = ((multiplesheets(path))$table) 
 
 extra_vars = c()
+extra_ind = c()
 for(i in 1:ncol(data)){
   temp = data[,i]
   len = length(unique(temp))
   if(len < 2){
     extra_vars = c(extra_vars, colnames(data)[i])
+    extra_ind = c(extra_ind, i)
   }
 }
 
