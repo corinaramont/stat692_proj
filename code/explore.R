c96adb66a4ffb9a3e942a0075cab64adb158b5d0
Corina Ramont <corinaramont@tamu.edu>
2024-02-17 20:08:06 -0600

    edit to read in new data

--- code/explore.R
+++ code/explore.R
@@ -1,31 +1,6 @@
 library(dplyr)
 
-source("functions/read_multiple_sheets.R") # load function
+data = read.csv("datasets/KMCC DM CRC.csv")
 
-# load on macOS
-path = "datasets/hai_only.xlsx"
-data = ((multiplesheets(path))$table) 
 
-# checking for variables that are empty
-extra_vars = c()
-extra_ind = c()
-for(i in 1:ncol(data)){
-  temp = data[,i]
-  len = length(unique(temp))
-  if(len < 2){
-    extra_vars = c(extra_vars, colnames(data)[i])
-    extra_ind = c(extra_ind, i)
-  }
-}
-extras = data.frame(col_num = extra_ind, var_name = extra_vars)
-
-# remove empty variables
-new = data[,-c(extra_ind)] # removes 100 empty variables
-# also pubmedid can def be rid of for analyses
-
-# summary stats
-table(new$gender) # 710 female, 783 male, 496 unknown
-table(new$country) # only 63 unknowns
-table(new$age) # only 546 unknowns
-table(new$disease)
 
