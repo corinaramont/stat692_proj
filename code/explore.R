1c9a194774faac1de1ed110cf7fdfa0a944313de
Corina Ramont <corinaramont@tamu.edu>
2024-02-22 11:24:26 -0600

    add comments and needed packages

--- code/explore.R
+++ code/explore.R
@@ -1,6 +1,12 @@
 library(dplyr)
+library(randomForest)
+library(caret)
 
 data = read.csv("datasets/KMCC DM CRC.csv")
+
+# time on study
 time_on_study = data$FU_AGE - data$E_AGE
+
+# random forest play time
 
 
