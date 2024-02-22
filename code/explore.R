0919424240e1b452db9a600cf5e9043ed665c262
Corina Ramont <corinaramont@tamu.edu>
2024-02-22 11:30:15 -0600

    add train and test split, set seed

--- code/explore.R
+++ code/explore.R
@@ -9,4 +9,11 @@ time_on_study = data$FU_AGE - data$E_AGE
 
 # random forest play time
 
+# 1. set seed
+set.seed(692)
 
+# 2. partition data for training and testing
+train_index = createDataPartition(data$CRC, p = .8, list = FALSE) %>% 
+  as.vector(.)
+train_data = data[train_index,]
+test_data = data[-train_index,]
